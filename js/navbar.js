d228b055c94607e1efca3abdfdbb9f0d91eb7e34
h-sarhan <hasoun7772@gmail.com>
2020-02-27 19:18:08 +0400

    Added contact us page

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,11 @@
 // This variable is an array representing all the navbar__link DOM objects
 var navbarLinks = document.querySelectorAll('.navbar__link');
 // This is an array of colors
-var colors = ['#2980b9', '#2ecc71', '#d35400'];
+var colors = ['#2980b9', '#2ecc71', '#d35400', '#5758BB'];
 
+var navbarExpand = document.querySelector('.navbar__expand');
+var showNav = navbarExpand.querySelector('.show-nav');
+var hideNav = navbarExpand.querySelector('.hide-nav');
 // This is the default background
 var defaultBackground = '';
 
@@ -20,3 +23,16 @@ navbarLinks.forEach((link, idx) => {
     link.style.background = defaultBackground;
   });
 });
+
+if (screen.width < 800) {
+  navbarExpand.addEventListener('click', function () {
+    navbarLinks.forEach((link, idx) => {
+      link.classList.toggle('collapsed');
+    })
+    navbarLinks[0].classList.remove('collapsed');
+    navbarLinks[0].classList.toggle('bottom-border');
+    showNav.classList.toggle('collapsed');
+    hideNav.classList.toggle('collapsed');
+
+  });
+}
